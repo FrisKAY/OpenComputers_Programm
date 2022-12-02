5815aed30cb0511cbaa3cc768777a4cc52ddc0ca
Alexander <makst79@gmail.com>
2022-12-02 04:47:26 +0200

    /

--- Reactor-core/installer.lua
+++ Reactor-core/installer.lua
@@ -1,10 +1,10 @@
 local shell = require("shell")
 local fs = require("filesystem")
-local prefix = "https://raw.githubusercontent.com/AlexCatze/RacoonNet/master/main/"
+local prefix = "https://raw.githubusercontent.com/FrisKAY/OpenComputers_Programm/master/Reactor-core/"
 local comp = require("computer")
-local files = {"/bin/chat.lua","/bin/chat_server.lua","/bin/ping.lua","/bin/rnconfig.lua","/bin/routconf.lua","/bin/router.lua","/bin/webserver.lua","/bin/wr.lua","/etc/config/sys.cfg","/etc/lang/ru.router.lang","/lib/opennet.lua","/lib/racoonnet.lua","/lib/rn_modem.lua","/lib/rn_stem.lua","/lib/rn_tunnel.lua",}
+local files = {"/bin/reactor.lua","/reactor-core/rcstart.lua","/reactor-core/rcstart98.lua","/reactor-core/rcstart99.lua","/reactor-core/rcstart-test.lua",}
 
-fs.makeDirectory("/etc/themes/")
+fs.makeDirectory("/reactor-core/")
 
 for _,v in pairs(files) do
   if not fs.exists(v:match(".*/")) then fs.makeDirectory(v:match(".*/")) end
